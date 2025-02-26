a0898a418b5e022aa43d04a770c32dfcd0f55699
stsaria <solothunder.autoer@gmail.com>
2025-02-26 20:04:12 +0900

    a

---
+++ src/main/java/si/f5/stsaria/crafterStrikeMain/CrafterStrikeMain.java
@@ -0,0 +1,17 @@
+package si.f5.stsaria.crafterStrikeMain;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrafterStrikeMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
